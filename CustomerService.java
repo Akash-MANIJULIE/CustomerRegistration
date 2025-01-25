0ebd6110ff0141d4300cf90ebed559729bd61e31
Akash Ramachandran <akashramachandran2003@gmail.com>
2025-01-25 14:14:13 +0530

    Add files via upload

---
+++ CustomerService.java
@@ -0,0 +1,21 @@
+package CustomerController;
+
+import java.util.List;
+
+public class CustomerService {
+    public String addCustomer(com.Project.SpringAngular.DTO.CustomerSaveDTO customerSaveDTO) {
+    }
+
+    public List<com.Project.SpringAngular.DTO.CustomerDTO> getAllCustomer() {
+    }
+
+    public String updateCustomers(com.Project.SpringAngular.DTO.CustomerUpdateDTO customerUpdateDTO) {
+    }
+
+    public boolean deleteCustomer(int id) {
+    }
+
+    public boolean deleteCustomer(int id) {
+        return false;
+    }
+}
